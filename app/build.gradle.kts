f69111b9b7759cbd45320452ccb9b68915827a6b
Alex Birkett <alex@birkett.no>
2024-12-10 18:11:24 +0100

    Use Compose Multiplaform compatible dependencies
    Replace Gson with kotlinx.serialization
    Replace java.util.Date with kotlinx.datetime.Instant

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    kotlin("plugin.serialization") version "2.0.20"
 }
 
 android {
@@ -40,7 +41,8 @@ android {
 }
 
 dependencies {
-    implementation("com.google.code.gson:gson:2.8.9")
+    implementation(libs.kotlinx.datetime)
+    implementation(libs.kotlinx.serialization.json)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
